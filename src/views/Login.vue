a1de1d7187535ef6b6eac94e2bcdd123a6e765b4
yanQAQ <845926906@qq.com>
2019-06-29 20:04:17 +0800

    login修改

---
+++ src/views/Login.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="login-container">
+    <div id="login-box">
+      <div id="logo-box">
+        <img src="../assets/img/logo.png" alt />
+      </div>
+      <el-form ref="loginFormRef" :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.username"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.userpass"></el-input>
+        </el-form-item>
+        <el-row>
+          <el-col push="15">
+            <el-button type="primary">登录</el-button>
+            <el-button type="info">重置</el-button>
+          </el-col>
+        </el-row>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // 用户登录表单数据对象(用户名、密码)
+      loginForm: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#login-container {
+  background-color: #2b4b6b;
+  height: 100%;
+  overflow: hidden;
+  #login-box {
+    width: 450px;
+    height: 304px;
+    background-color: #fff;
+    border-radius: 4px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    .el-form {
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      padding: 20px;
+      box-sizing: border-box;
+    }
+    #logo-box {
+      width: 130px;
+      height: 130px;
+      border: 1px solid #eee;
+      border-radius: 50%;
+      padding: 8px;
+      box-shadow: 0 0 10px #eee;
+      position: absolute;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #fff;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true, // 在保存代码的时候开启eslint代码检查机制
+  devServer: { // 实时保存、编译的配置段
+    open: true,
+    port: 12306
+  }
 }
