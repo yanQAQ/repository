8f37fd3ba92439de97072b089768d3a63007192f
yanQAQ <845926906@qq.com>
2019-07-02 21:12:31 +0800

    修复login页面错误警告

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -14,7 +14,7 @@
           <el-input v-model="loginForm.password" show-password></el-input>
         </el-form-item>
         <el-row>
-          <el-col push="15">
+          <el-col>
             <el-button type="primary" @click="login()">登录</el-button>
             <el-button type="info" @click="$refs.loginFormRef.resetFields()">重置</el-button>
           </el-col>
