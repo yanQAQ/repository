d31a60bd9f34ac3fd90ef3577d08b50964c89baa
YanHaoYuan <845926906@qq.com>
2020-04-24 11:26:12 +0800

    简约风

--- admin/src/views/Navbar.vue
+++ admin/src/views/Navbar.vue
@@ -44,14 +44,6 @@
               <i class="el-icon-location"></i>
               <span slot="title">导航一</span>
             </template>
-            <el-menu-item-group>
-              <span slot="title">分组一</span>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
             <el-submenu index="1-4">
               <span slot="title">选项4</span>
               <el-menu-item index="1-4-1">选项1</el-menu-item>
@@ -61,7 +53,7 @@
             <i class="el-icon-menu"></i>
             <span slot="title">导航二</span>
           </el-menu-item>
-          <el-menu-item index="3" disabled>
+          <el-menu-item index="3">
             <i class="el-icon-document"></i>
             <span slot="title">导航三</span>
           </el-menu-item>
@@ -132,13 +124,13 @@ export default class Navbar extends Vue {
 .el-header {
   padding: 0;
 }
-.el-menu-demo {
-  box-shadow: 0px  2px 2px #ccc ;
-  border: none;
+.el-menu-demo:hover {
+  box-shadow: 0px  2px 2px rgba(222,222,222,1);
+}
+.el-menu-vertical-demo:hover {
+  box-shadow: 2px  0px 2px rgba(222,222,222,1) ;
 }
 .el-menu-vertical-demo {
-  box-shadow: 2px  0px 2px #ccc ;
-  border:none;
   height:calc(100vh - 60px);
 }
 </style>
